0dace4208cc59865fcf764b2158e810687d2587f
Leandro Lisboa Penz <lpenz@lpenz.org>
2022-12-01 21:59:50 +0000

    Add current flake.nix/lock to repository

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "lpenz's home-manager config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    execpermfix.url = "github:lpenz/execpermfix";
+    tuzue.url = "github:lpenz/tuzue";
+  };
+
+  outputs = { nixpkgs, home-manager, execpermfix, tuzue, ... }:
+    let
+      system = "x86_64-linux";
+      user = "lpenz";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations.lpenz = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          {
+            programs.home-manager.enable = true;
+            home.stateVersion = "22.11";
+            home.username = "${user}";
+            home.homeDirectory = "/home/${user}";
+            home.packages = [
+              execpermfix.packages.${system}.default
+              tuzue.packages.${system}.default
+
+              pkgs.bat
+              pkgs.fd
+              pkgs.fzf
+              pkgs.glibcLocales
+              pkgs.ripgrep
+              pkgs.topgrade
+              pkgs.zsh
+            ];
+            programs.emacs.enable = true;
+          }
+        ];
+      };
+    };
+}
