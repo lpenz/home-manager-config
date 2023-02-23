cb48b72021ed74f1b2353cff70b556363dc0ad55
Leandro Lisboa Penz <lpenz@lpenz.org>
2023-02-23 20:05:55 +0000

    config.fish: set some environment variables

--- flake.nix
+++ flake.nix
@@ -58,8 +58,16 @@
               interactiveShellInit = ''
                 # Commands to run in interactive sessions can go here
                 set fish_greeting
+                set -Ux LOCALE_ARCHIVE_2_27 $HOME/.nix-profile/lib/locale/locale-archive
                 set -gx EDITOR vim
                 set -gx VISUAL vim
+                set -gx LC_ALL en_US.UTF-8
+                set -gx LANGUAGE en
+                set -gx PYTHONIOENCODING UTF-8
+                set -gx NIX_IGNORE_SYMLINK_STORE 1
+                set -gx PYTHONSTARTUP $HOME/.pystartup
+                set -gx XAUTHORITY $HOME/.Xauthority
+                set -gx RUST_SRC_PATH /usr/src/rust/src
                 direnv hook fish | source
                 function push-line
                     commandline -f kill-whole-line
