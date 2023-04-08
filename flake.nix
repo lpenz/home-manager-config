adc58775ca6e6fe547298b4d2a5f0c563fc0c959
Leandro Lisboa Penz <lpenz@lpenz.org>
2023-04-08 18:59:48 +0100

    Use nix 22.11 as base instead of unstable
    Should make updates less volumous.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "lpenz's home-manager config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
