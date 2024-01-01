132e29bb75c381bc608325c3badec3258a65015d
Leandro Lisboa Penz <lpenz@lpenz.org>
2024-01-01 18:03:03 +0000

    flake.nix: unpin stdecor, upgrade to 0.1.10

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     disk-img-tool.url = "github:lpenz/disk-img-tool";
     execpermfix.url = "github:lpenz/execpermfix";
     ogle.url = "github:lpenz/ogle";
-    stdecor.url = "github:lpenz/stdecor/v0.1.6";
+    stdecor.url = "github:lpenz/stdecor";
   };
 
   outputs = { nixpkgs, home-manager, cachix, disk-img-tool, execpermfix, ogle, stdecor, ... }:
