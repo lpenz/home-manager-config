1e5834637a24bda31cd6163a0d301d7ed74482d1
Leandro Lisboa Penz <lpenz@lpenz.org>
2025-01-16 15:48:43 +0000

    Fix and make nix-install more thorough
    Also set use-sqlite-wal=false in nix.conf
    (which requires a reinstall)

--- nix.conf.nix
+++ nix.conf.nix
@@ -3,8 +3,8 @@
   trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" "lpenz.org:jYDkTN/2qS96NxFnJpuWkNdVfNaWzaA5pmcg4MyXa2g=" "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" "lpenz.cachix.org-1:ZRfvZ7o/oNiBcD9EtGKpzZSgJKG89uFZn/36zbCh1Oo=" ];
   experimental-features = [ "nix-command" "flakes" ];
   allow-symlinked-store = true;
-  # use-sqlite-wal = false;
+  use-sqlite-wal = false;
   cores = 1;
-  # max-substitution-jobs = 1;
+  max-substitution-jobs = 1;
   fsync-metadata = false;
 }
