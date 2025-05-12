137770ae6ca4980b9a8215a397bcc57ecff79af5
Leandro Lisboa Penz <lpenz@lpenz.org>
2025-05-11 21:34:37 -0300

    fish.nix: move functions from init to the functions option

--- fish.nix
+++ fish.nix
@@ -24,72 +24,74 @@
     set --prepend fish_function_path ${pkgs.fzf}/share/fish/vendor_functions.d
     fzf_key_bindings
     direnv hook fish | source
-    function push-line
-        commandline -f kill-whole-line
-        function restore_line --on-event fish_prompt
-            commandline -f yank
-            functions -e restore_line
-        end
-    end
-    bind \eq push-line
+    bind \eq fish-push-line
     bind \ed kill-bigword
     bind \e\x7f backward-kill-bigword
     bind \eb backward-bigword
     bind \cx\ce edit_command_buffer
-    function fish-omnibuild
-        commandline -r omnibuild
-        commandline -f execute
-    end
     bind -k f10 fish-omnibuild
-    function fish-append-cwd
-        commandline -i {$PWD}/
-    end
     bind \ew fish-append-cwd
-    function fish-sudo-line
-        fish_commandline_prepend sudo
-    end
     bind \e1 fish-sudo-line
     bind \e! fish-sudo-line
-    function fish-repeat-last-word
-        set -l cmdline (commandline)
-        set -l cursor_pos (commandline -C)
-
-        # Move cursor to the end of the current word if inside a word
-        set -l end_of_word_pos (string match -r -i '[^ ]*$' -- (string sub -s $cursor_pos -- $cmdline) | string length)
-        if test $end_of_word_pos -gt 0
-            set cursor_pos (math $cursor_pos + $end_of_word_pos - 0)
-        end
-
-        set -l left_part (string sub -l $cursor_pos -- $cmdline)
-        set -l right_part (string sub -s (math $cursor_pos + 1) -- $cmdline)
-
-        set -l words_in_left (string split " " -- $left_part)
-        set -l last_word
-
-        # Iterate over the words in reverse to find the last non-empty word
-        for word in (seq (count $words_in_left) -1 1)
-            set -l current_word (string trim -c ' ' -- $words_in_left[$word])
-            if test -n "$current_word"
-                set last_word $current_word
-                break
-            end
-        end
-
-        # Duplicate the word if found
-        if test -n "$last_word"
-            commandline -r -- "$left_part $last_word$right_part"
-            commandline -C (math $cursor_pos + (string length -- $last_word) + 1)
-        end
-    end
     bind \em fish-repeat-last-word
     if test -e "$HOME/.fishrc-local.fish"
         source "$HOME/.fishrc-local.fish"
     end
-    function fish-pager-vim
-        fish_commandline_append ' 2>&1 | vi -'
-    end
     bind \ei fish-pager-vim
   '';
+  functions = {
+    fish-push-line.body = ''
+      commandline -f kill-whole-line
+      function restore_line --on-event fish_prompt
+          commandline -f yank
+          functions -e restore_line
+      end
+    '';
+    fish-omnibuild = ''
+      commandline -r omnibuild
+      commandline -f execute
+    '';
+    fish-append-cwd = ''
+      commandline -i {$PWD}/
+    '';
+    fish-sudo-line = ''
+      fish_commandline_prepend sudo
+    '';
+    fish-pager-vim = ''
+      fish_commandline_append ' 2>&1 | vi -'
+    '';
+    fish-repeat-last-word = ''
+      set -l cmdline (commandline)
+      set -l cursor_pos (commandline -C)
+
+      # Move cursor to the end of the current word if inside a word
+      set -l end_of_word_pos (string match -r -i '[^ ]*$' -- (string sub -s $cursor_pos -- $cmdline) | string length)
+      if test $end_of_word_pos -gt 0
+          set cursor_pos (math $cursor_pos + $end_of_word_pos - 0)
+      end
+
+      set -l left_part (string sub -l $cursor_pos -- $cmdline)
+      set -l right_part (string sub -s (math $cursor_pos + 1) -- $cmdline)
+
+      set -l words_in_left (string split " " -- $left_part)
+      set -l last_word
+
+      # Iterate over the words in reverse to find the last non-empty word
+      for word in (seq (count $words_in_left) -1 1)
+          set -l current_word (string trim -c ' ' -- $words_in_left[$word])
+          if test -n "$current_word"
+              set last_word $current_word
+              break
+          end
+      end
+
+      # Duplicate the word if found
+      if test -n "$last_word"
+          commandline -r -- "$left_part $last_word$right_part"
+          commandline -C (math $cursor_pos + (string length -- $last_word) + 1)
+      end
+    '';
+  };
   plugins = [
     { name = "sponge"; src = pkgs.fishPlugins.sponge.src; }
     { name = "tide"; src = pkgs.fishPlugins.tide.src; }
