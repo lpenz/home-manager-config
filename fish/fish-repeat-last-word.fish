aecdde3c0c4c5f3cbcaadec4d0cb9322c02d4031
Leandro Lisboa Penz <lpenz@lpenz.org>
2025-08-02 22:15:41 +0100

    Fix fish-repeat-last-word.fish

--- fish/fish-repeat-last-word.fish
+++ fish/fish-repeat-last-word.fish
@@ -1,29 +1,39 @@
-set -l cmdline (commandline)
-set -l cursor_pos (commandline -C)
+set cmd (commandline)
+set pos (commandline -C)
+set len (string length -- $cmd)
 
-# Move cursor to the end of the current word if inside a word
-set -l end_of_word_pos (string match -r -i '[^ ]*$' -- (string sub -s $cursor_pos -- $cmdline) | string length)
-if test $end_of_word_pos -gt 0
-    set cursor_pos (math $cursor_pos + $end_of_word_pos - 0)
-end
-
-set -l left_part (string sub -l $cursor_pos -- $cmdline)
-set -l right_part (string sub -s (math $cursor_pos + 1) -- $cmdline)
-
-set -l words_in_left (string split " " -- $left_part)
-set -l last_word
-
-# Iterate over the words in reverse to find the last non-empty word
-for word in (seq (count $words_in_left) -1 1)
-    set -l current_word (string trim -c ' ' -- $words_in_left[$word])
-    if test -n "$current_word"
-        set last_word $current_word
+# Move cursor right until we hit a space (word end)
+while test $pos -lt $len
+    set char (string sub -s (math $pos + 1) -l 1 -- $cmd)
+    if test "$char" = ' '
         break
     end
+    set pos (math $pos + 1)
 end
 
-# Duplicate the word if found
-if test -n "$last_word"
-    commandline -r -- "$left_part $last_word$right_part"
-    commandline -C (math $cursor_pos + (string length -- $last_word) + 1)
+# Move cursor left to first non-space
+while test $pos -gt 0
+    set char (string sub -s $pos -l 1 -- $cmd)
+    if test "$char" != ' '
+        break
+    end
+    set pos (math $pos - 1)
 end
+
+# Text before and after this point
+set before (string sub -l $pos -- $cmd)
+set after (string sub -s (math $pos + 1) -- $cmd)
+
+# Extract the last "bigword" (non-whitespace sequence)
+set word (string match -r '[^ \t\n\r\f\v]+$' -- $before)
+if test -z "$word"
+    echo "No word to duplicate."
+    return 1
+end
+
+# Insert a space and then the duplicate of the word
+set new_before "$before $word"
+
+# Replace the commandline and adjust cursor
+commandline -r "$new_before$after"
+commandline -C (string length -- $new_before)
