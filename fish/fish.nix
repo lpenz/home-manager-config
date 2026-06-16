1e23b380fa7522c66877855ae9b3e7ca188766d4
Leandro Lisboa Penz <lpenz@lpenz.org>
2026-06-16 18:33:29 +0100

    feat: add $HOME/.local/bin to the fish path

--- fish/fish.nix
+++ fish/fish.nix
@@ -7,6 +7,7 @@
     # Install fundle
     if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
     fish_config theme choose 'fish default'
+    fish_add_path "$HOME/.local/bin"
     fish_add_path "$HOME/bin"
     # Fix tide falling through to vi-mode: (remove once tide is fixed)
     set -gx fish_key_bindings fish_default_key_bindings
